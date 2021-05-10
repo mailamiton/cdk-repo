6738d9e33a3020f2403243cfdb22b55aad37c291
EC2 Default User <ec2-user@ip-172-31-40-35.eu-north-1.compute.internal>
2021-05-10 06:52:50 +0000

    added changes for applicaiotn load balancer

--- README.md
+++ README.md
@@ -1,1 +1,34 @@
 # cdk-repo
+https://cicd-pipeline-cdk-eks-bluegreen.workshop.aws/en/intro/prepare.html
+
+(a) AWS CLI Installed:
+sudo yum install -y jq
+export ACCOUNT_ID=$(aws sts get-caller-identity --output text --query Account)
+export AWS_REGION=$(curl -s 169.254.169.254/latest/dynamic/instance-identity/document | jq -r '.region')
+echo "export ACCOUNT_ID=${ACCOUNT_ID}" | tee -a ~/.bash_profile
+echo "export AWS_REGION=${AWS_REGION}" | tee -a ~/.bash_profile
+aws configure set default.region ${AWS_REGION}
+aws configure get default.regions
+
+(b) Download and install Kubectl and IAM Authenticator:
+curl -LO https://storage.googleapis.com/kubernetes-release/release/v1.17.0/bin/linux/amd64/kubectl
+chmod +x ./kubectl
+sudo mv ./kubectl /usr/local/bin/kubectl
+kubectl help
+curl -o aws-iam-authenticator https://amazon-eks.s3.us-west-2.amazonaws.com/1.15.10/2020-02-22/bin/linux/amd64/aws-iam-authenticator
+chmod +x ./aws-iam-authenticator
+mkdir -p $HOME/bin && cp ./aws-iam-authenticator $HOME/bin/aws-iam-authenticator && export PATH=$PATH:$HOME/bin
+echo 'export PATH=$PATH:$HOME/bin' >> ~/.bashrc
+
+c)  Install pre-requisite packages for the CDK:
+sudo yum install -y npm
+npm install -g aws-cdk@1.30.0 --force
+npm install -g typescript@latest
+
+
+d) CDK Setup :-
+cdk doctor
+cdk init --language typescript
+npm run build
+cdk synth
+cdk deploy
---
+++ app-loadbalancer/lib/app-loadbalancer-stack.ts
@@ -0,0 +1,56 @@
+import * as cdk from '@aws-cdk/core';
+import * as autoscaling from '@aws-cdk/aws-autoscaling'
+import * as ec2 from  '@aws-cdk/aws-ec2'
+import * as elbv2 from '@aws-cdk/aws-elasticloadbalancingv2'
+
+
+export class AppLoadbalancerStack extends cdk.Stack {
+  constructor(scope: cdk.Construct, id: string, props?: cdk.StackProps) {
+    super(scope, id, props);
+
+    // The code that defines your stack goes here
+    const vpc = new ec2.Vpc(this, 'VPC');
+    const linux = new ec2.GenericLinuxImage({
+      'eu-north-1': 'ami-05dcbf8b36d111036'
+    });
+
+    const mySG = new ec2.SecurityGroup(this, '${stack}-ssh-app', {
+        vpc: vpc,
+        allowAllOutbound: true,
+        description: 'Security Group '
+    });
+
+    mySG.addIngressRule(ec2.Peer.anyIpv4(), ec2.Port.tcp(22), 'SSH frm anywhere');
+    mySG.addIngressRule(ec2.Peer.anyIpv4(), ec2.Port.tcp(80), 'For Application Deployment');
+    //mySG.addIngressRule(ec2.Peer.ipv4('10.200.0.0/24'), ec2.Port.tcp(80), 'Redshift Ingress1');
+
+    const asg = new autoscaling.AutoScalingGroup(this, 'ASG', {
+      vpc,
+      instanceType: ec2.InstanceType.of(ec2.InstanceClass.T3, ec2.InstanceSize.MICRO),
+      machineImage: linux,
+      minCapacity: 1,
+      maxCapacity: 5
+    });
+    asg.addSecurityGroup(mySG)
+
+    const lb = new elbv2.ApplicationLoadBalancer(this, 'LB', {
+      vpc,
+      internetFacing: true
+    });
+
+    const listener = lb.addListener('Listener', {
+      port: 80,
+    });
+
+    listener.addTargets('Target', {
+      port: 80,
+      targets: [asg]
+    });
+
+    listener.connections.allowDefaultPortFromAnyIpv4('Open to the world');
+
+    asg.scaleOnRequestCount('AModestLoad', {
+      targetRequestsPerSecond: 1
+    });
+  }
+}
